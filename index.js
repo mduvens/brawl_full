38151a9593b42cb785bb0e2e633dcab745e8f666
manuel <mduvens@gmail.com>
2021-10-10 18:58:46 +0100

    pc

--- index.js
+++ index.js
@@ -5,22 +5,11 @@ const cors = require('cors')
 const brawlApi = require('brawl-stars-api')
 
 
-var allowedOrigins = ['http://176.78.165.210',
-                      'http://brawlers.kozow.com'];
-app.use(cors({  
-  origin: function(origin, callback){
-    // allow requests with no origin     
-    // (like mobile apps or curl requests)    
-    if(!origin) 
-      return callback(null, true);    
-    if(allowedOrigins.indexOf(origin) === -1){
-      var msg = 'The CORS policy for this site does not ' +                
-          'allow access from the specified Origin.';      
-      return callback(new Error(msg), false);    
-    }    
-    return callback(null, true);  
-  }
-}));
+app.use(function(req, res, next){
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+  });
+  
 
 const PLAYER_TAGS = {
   Manel: '#2GPPL89QY',
